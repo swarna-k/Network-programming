05241d9da08b7c7216f3e157f03bac5a6af6aca8
Mandel Oats <oats456@gmail.com>
2015-09-28 18:37:12 -0500

    Seperated into header and implementation files

---
+++ Assignment_1/encoding.h
@@ -0,0 +1,19 @@
+
+
+typedef struct msg_header {
+	unsigned short vrsn : 9 ; //9 bits
+	unsigned short type : 7 ; //7 bits
+	unsigned short length : 16 ; //16 bits
+}msg_header;
+
+typedef struct attr_header {
+	unsigned short type : 16;
+	unsigned short length : 16;
+}attr_header;
+
+void msg_header_hton(msg_header *msg_hdr);
+void msg_header_ntoh( msg_header *msg_hdr);
+void attr_header_hton(attr_header *attr_hdr);
+void attr_header_ntoh(attr_header *attr_hdr);
+char* encode_msg(msg_header *msg_hdr, attr_header *attr_hdr, attr_header *attr_hdr2, char payload[], char payload2[]); // 2 can be NULL
+void decode_msg(char *msg, msg_header *msg_hdr, attr_header *attr_hdr, attr_header *attr_hdr2, char payload[], char payload2[]); //2 can be NULL
